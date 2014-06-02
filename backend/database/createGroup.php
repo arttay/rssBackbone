5ec594951d34d0f2fc5792c309567da2477d1cda
arttay <arttay1090@yahoo.com>
2014-06-02 14:00:40 -0500

    going home commit

--- backend/database/createGroup.php
+++ backend/database/createGroup.php
@@ -5,7 +5,7 @@
 	$checkSql = "SELECT `GroupName` FROM `$userGroups` WHERE GroupName = '$userName'";
 	$checkQuery = mysqli_query($link, $checkSql);
 	$checkRow = mysqli_fetch_assoc($checkQuery);
-	if($checkRow > 0){
+	if($checkRow == false){
 		$sql="INSERT INTO `rss`.`$userGroups` (`GroupName`, `links`) VALUES ('$groupName', '$feed')";
 	mysqli_query($link, $sql)or die("A MySQL error has occurred.<br />Error: (" . mysql_errno() . ") " . mysql_error()."28");
 	}
